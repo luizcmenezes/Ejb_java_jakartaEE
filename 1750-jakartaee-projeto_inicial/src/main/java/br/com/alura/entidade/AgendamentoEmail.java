6b8fa3e5280a0d2a815f94710664259e0afd742c
luizcmenezes <lcmscontas@gmail.com>
2020-11-23 20:12:05 -0300

    Mensageria com JMS

--- 1750-jakartaee-projeto_inicial/src/main/java/br/com/alura/entidade/AgendamentoEmail.java
+++ 1750-jakartaee-projeto_inicial/src/main/java/br/com/alura/entidade/AgendamentoEmail.java
@@ -1,12 +1,16 @@
 package br.com.alura.entidade;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class AgendamentoEmail {
+public class AgendamentoEmail implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- 1750-jakartaee-projeto_inicial/src/main/java/br/com/alura/job/AgendamentoEmailJob.java
+++ 1750-jakartaee-projeto_inicial/src/main/java/br/com/alura/job/AgendamentoEmailJob.java
@@ -2,9 +2,13 @@ package br.com.alura.job;
 
 import java.util.List;
 
+import javax.annotation.Resource;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.inject.Inject;
+import javax.jms.JMSConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.Queue;
 
 import br.com.alura.entidade.AgendamentoEmail;
 import br.com.alura.servico.AgendamentoEmailServico;
@@ -14,13 +18,20 @@ public class AgendamentoEmailJob {
 
     @Inject
     private AgendamentoEmailServico agendamentoEmailServico;
+    
+    @Inject
+    @JMSConnectionFactory("java:jboss/DefaultJMSConnectionFactory")
+    private JMSContext context;
+    
+    @Resource(mappedName = "java:/jms/queue/EmailQueue")
+    private Queue queue;
 
     @Schedule(hour = "*", minute = "*", second = "*/10")
     public void enviarEmail() {
         List<AgendamentoEmail> list = agendamentoEmailServico.listarPorNaoAgendado();
 
         list.forEach(emailNaoAgendado -> {
-            agendamentoEmailServico.enviar(emailNaoAgendado);
+            context.createProducer().send(queue, emailNaoAgendado);
             agendamentoEmailServico.alterar(emailNaoAgendado);
         });
     }
